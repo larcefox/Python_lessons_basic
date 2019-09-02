58026d4b1bfbc1513ca25eb3d47aab1c3f289f2e
larcefox <larcethefox@gmail.com>
2019-09-02 09:31:35 +0300

    HW 6: Easy and Normal levels.

--- lesson06/home_work/hw06_easy.py
+++ lesson06/home_work/hw06_easy.py
@@ -1,5 +1,39 @@
 # Задача-1: Написать класс для фигуры-треугольника, заданного координатами трех точек.
 # Определить методы, позволяющие вычислить: площадь, высоту и периметр фигуры.
+import math
+
+class Triangle():
+
+    def __init__(self, coord_a, coord_b, coord_c):
+
+        self.coord_a = coord_a
+        self.coord_b = coord_b
+        self.coord_c = coord_c
+
+        self.side_a = math.sqrt((coord_b[0] - coord_a[0]) ** 2 + (coord_b[1] - coord_a[1]) ** 2)
+        self.side_b = math.sqrt((coord_c[0] - coord_b[0]) ** 2 + (coord_c[1] - coord_b[1]) ** 2)
+        self.side_c = math.sqrt((coord_a[0] - coord_c[0]) ** 2 + (coord_a[1] - coord_c[1]) ** 2)
+        self.half_p = (self.side_a + self.side_b + self.side_c) / 2
+
+
+    def square(self):
+        return (self.height() * self.side_a / 2)
+    def height(self):
+        return (2 * math.sqrt(self.half_p * ((self.half_p - self.side_a) * (self.half_p - self.side_b) * (self.half_p - self.side_c))) / self.side_a)
+    def perimeter(self):
+        return (self.side_a + self.side_b + self.side_c)
+
+coord_tri_a = (1, 2)
+coord_tri_b = (-1, 1)
+coord_tri_c = (2, 2)
+
+triang = Triangle(coord_tri_a, coord_tri_b, coord_tri_c)
+
+# print(triang.side_a, triang.side_b, triang.side_c)
+# print(triang.square())
+# print(triang.height())
+# print(triang.perimeter())
+
 
 
 # Задача-2: Написать Класс "Равнобочная трапеция", заданной координатами 4-х точек.
@@ -7,3 +41,50 @@
 # проверка, является ли фигура равнобочной трапецией;
 # вычисления: длины сторон, периметр, площадь.
 
+class Trapeze():
+
+    def __init__(self, coord_a, coord_b, coord_c, coord_d):
+        self.coord_a = coord_a
+        self.coord_b = coord_b
+        self.coord_c = coord_c
+        self.coord_d = coord_d
+
+        self.side_a = math.sqrt((coord_b[0] - coord_a[0]) ** 2 + (coord_b[1] - coord_a[1]) ** 2)
+        self.side_b = math.sqrt((coord_c[0] - coord_b[0]) ** 2 + (coord_c[1] - coord_b[1]) ** 2)
+        self.side_c = math.sqrt((coord_d[0] - coord_c[0]) ** 2 + (coord_d[1] - coord_c[1]) ** 2)
+        self.side_d = math.sqrt((coord_a[0] - coord_d[0]) ** 2 + (coord_a[1] - coord_d[1]) ** 2)
+
+    @property
+    def perimeter(self):
+        return (self.side_a + self.side_b + self.side_c + self.side_d)
+
+    @property
+    def square(self):
+        return (self.side_b + self.side_d / 4 * math.sqrt(4 * self.side_b - (self.side_b - self.side_d) ** 2))
+
+    @property
+    def isosceles(self):
+        return True if self.side_a == self.side_c else False
+
+'''
+        A     d          D
+        .________________.
+       /                  \
+     a/                    \c
+     /                      \
+    .________________________.
+   B         b                C
+'''
+
+
+
+coord_trap_a = (-1, 1)
+coord_trap_b = (-2, -1)
+coord_trap_c = (2, -1)
+coord_trap_d = (1, 1)
+
+trap = Trapeze(coord_trap_a, coord_trap_b, coord_trap_c, coord_trap_d)
+
+print(trap.isosceles)
+print(trap.square)
+print(trap.perimeter)
--- lesson06/home_work/hw06_normal.py
+++ lesson06/home_work/hw06_normal.py
@@ -7,6 +7,52 @@
 # Т.е. Учитель Иванов может преподавать математику у 5А и 6Б,
 # но больше математику не может преподавать никто другой.
 
+class People:
+    def __init__(self, name, patronymic, surname, birth_date, school, sex):
+        self.name = name
+        self.surname = surname
+        self.birth_date = birth_date
+        self.school = school
+        self.sex = sex
+        self.patronymic = patronymic
+
+    def get_full_name(self):
+        return self.name + ' ' + self.surname
+
+    def set_name(self, new_name):
+        self.name = new_name
+
+
+class Student(People):
+    def __init__(self, name, patronymic, surname, birth_date, school, sex, class_room, parents):
+        # Явно вызываем конструктор родительского класса
+        People.__init__(self, name, patronymic, surname, birth_date, school, sex)
+        # Добавляем уникальные атрибуты
+        self._class_room = {'class_num': int(class_room.split()[0]),
+                            'class_char': class_room.split()[1]}
+        self.parents = {'mother': parents[0],
+                        'father': parents[1]}
+    # И уникальные методы
+    @property
+    def class_room(self):
+        return "{} {}".format(self._class_room['class_num'], self._class_room['class_char'])
+
+    def next_class(self):
+        self._class_room['class_num'] += 1
+
+    def get_full_name(self):
+        return self.name + ' ' + self.surname
+
+    def get_fio(self):
+        return str((self.surname, " ", self.name[0], ".", self.patronymic[0], "."))
+
+
+class Teacher(People):
+    def __init__(self, name, patronymic, surname, birth_date, school, sex, teach_classes, subject):
+        People.__init__(self, name, patronymic, surname, birth_date, school, sex)
+        self.teach_classes = teach_classes
+        self.subject = subject
+
 # Выбранная и заполненная данными структура должна решать следующие задачи:
 # 1. Получить полный список всех классов школы
 # 2. Получить список всех учеников в указанном классе
@@ -15,3 +61,65 @@
 #  (Ученик --> Класс --> Учителя --> Предметы)
 # 4. Узнать ФИО родителей указанного ученика
 # 5. Получить список всех Учителей, преподающих в указанном классе
+
+students = []
+teachers = []
+
+
+def get_clases():
+    # 1. Получить полный список всех классов школы
+    classes = set()
+    for student in students:
+        classes.add(student.class_room)
+    return classes
+
+
+
+def students_by_class(current_class):
+
+    for student in students:
+        if student.class_room == current_class:
+            return student.get_fio()
+
+
+def get_subjects(current_student):
+    # 3. Получить список всех предметов указанного ученика
+    #  (Ученик --> Класс --> Учителя --> Предметы)
+    current_teachers = []
+    current_subjects = []
+
+    for student in students:
+
+        if (student.surname, " ", student.name, " ", student.patronymic) == current_student:
+            current_class = student.class_room
+
+    try:
+        for teacher in teachers:
+            if current_class in teacher.teach_classes:
+                current_teachers.append(teacher)
+    except UnboundLocalError:
+
+        return ('Ученик не найден')
+
+    for current_teacher in current_teachers:
+        current_subjects.append(current_teacher.subject)
+
+    return current_subjects
+
+
+def get_parents(current_student):
+    # 4. Узнать ФИО родителей указанного ученика
+    for student in students:
+
+        if (student.surname, " ", student.name, " ", student.patronymic) == current_student:
+            return student.parents
+
+
+def get_teachers_by_class(current_class):
+    # 5. Получить список всех Учителей, преподающих в указанном классе
+    current_teachers = []
+    for teacher in teachers:
+        if current_class in teacher.teach_classes:
+            current_teachers.append(teacher)
+
+    return current_teachers
